de2b00b5c5f6c36bc2f04aff78ea5efab8614b35
nathaniel may <nathanieldmay@gmail.com>
2017-12-12 09:15:49 -0600

    fixed promise returns for getCol and deleting docs

--- js-es6/MongoDal.js
+++ js-es6/MongoDal.js
@@ -123,32 +123,36 @@ class MongoDal{
   };
 
   countCol(){
-    let fn = () => {
-      return this.dalExample.count(); //TODO add $comment
-    };
+    return new Promise((resolve, reject) => {
+      let fn = () => {
+        return this.dalExample.count({}); //TODO add $comment
+      };
 
-    this._retryOnErr(fn).then((count) => {
-      logger.debug(new Date() + ' ' + this.logModule + ' collection count is ' + count);
-      resolve(count);
+      this._retryOnErr(fn).then((count) => {
+        logger.debug(new Date() + ' ' + this.logModule + ' collection count is ' + count);
+        resolve(count);
+      })
+      .catch((err) => {
+        logger.error(new Date() + ' ' + this.logModule + ' error counting collection: ' + err);
+        reject(err);
+      });
     })
-    .catch((err) => {
-      logger.error(new Date() + ' ' + this.logModule + ' error getting by id');
-      reject(err);
-    });
   }
 
   deleteAllDocs(){
-    let fn = () => {
-      return this.dalExample.deleteMany({}); //TODO add $comment
-    };
+    return new Promise((resolve, reject) => {
+      let fn = () => {
+        return this.dalExample.deleteMany({}); //TODO add $comment
+      };
 
-    this._retryOnErr(fn).then(() => {
-      logger.debug(new Date() + ' ' + this.logModule + ' found ' + array.length + ' docs');
-      resolve();
-    })
-    .catch((err) => {
-      logger.error(new Date() + ' ' + this.logModule + ' error getting by id');
-      reject(err);
+      this._retryOnErr(fn).then(() => {
+        logger.debug(new Date() + ' ' + this.logModule + ' found ' + array.length + ' docs');
+        resolve();
+      })
+      .catch((err) => {
+        logger.error(new Date() + ' ' + this.logModule + ' error getting by id');
+        reject(err);
+      });
     });
   }
 
