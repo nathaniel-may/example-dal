d9b7d30d4600db24fbec6d41c7de5e8a4fd80e2d
nathaniel may <nathanieldmay@gmail.com>
2018-01-09 14:39:23 -0500

    uses self correctly and logs events

--- python3/mongo_dal.py
+++ python3/mongo_dal.py
@@ -1,31 +1,42 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from datetime import datetime
+import logging.config
 
 class MongoDal:
-	'''example functions for accessing mongodb'''
+    '''example functions for accessing mongodb'''
 
-	def __init__(self, connStr, logLvl):
-		global connString
-		connString = connStr
-		global logLevel
-		logLevel = logLvl
-		global client
-		client = None
+    def __init__(self, connString, logLevel):
 
-	def init(self):
-		global client
-		if(client is None):
-			global connString
-			client = MongoClient(connString)
-		db = client.dalExample
-		#create all collections
-		global dalExample
-		dalExample = db.example
+        #logging
+        self.logger = logging.getLogger('DAL')
+        self.logger.setLevel(logLevel)
+        handler = logging.StreamHandler()
+        handler.setLevel(logLevel)
+        formatter = logging.Formatter('%(asctime)s\t%(name)s\t\t%(levelname)s::%(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-	def insertDoc(self, doc):
-		id = ObjectId()
-		doc.update({'_id': id})
-		#TODO RETRY and ERROR HANDLE
-		global dalExample
-		dalExample.insert_one(doc)
-		return id;
+        #set variables for connecting to mongo
+        self.connString = connString
+        self.client = None
+        self.logger.debug('completed __init__')
+
+    def init(self):
+        self.logger.debug('started init()')
+        if(self.client is None):
+            self.client = MongoClient(self.connString)
+        db = self.client.dalExample
+        #create all collections
+        self.dalExample = db.example
+        self.logger.debug('completed init()')
+
+    def insertDoc(self, doc):
+        self.logger.debug('started insertDoc()')
+        id = ObjectId()
+        doc.update({'_id': id})
+        #TODO RETRY and ERROR HANDLE
+        self.dalExample.insert_one(doc)
+        self.logger.debug('completed insertDoc()')
+        return id;
+
--- python3/mongo_dal_test.py
+++ python3/mongo_dal_test.py
@@ -1,16 +1,32 @@
 from mongo_dal import MongoDal
 from bson.objectid import ObjectId
+from datetime import datetime
 import unittest
+import logging
 
 class MongoDalTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        global dal
-        dal = MongoDal('mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=repl0&w=majority', 'silly')
-        dal.init()
+
+        #logging
+        cls.logger = logging.getLogger('DAL TEST')
+        cls.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s\t%(name)s\t%(levelname)s::%(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+        handler.setFormatter(formatter)
+        cls.logger.addHandler(handler)
+
+        #connect to MongoDB
+        cls.logger.debug('----------setUpClass----------')
+        cls.dal = MongoDal('mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=repl0&w=majority', logging.DEBUG)
+        cls.dal.init()
+        cls.logger.debug('----------setUpClass----------\n')
+
 
     def testInsertingOneDoc(self):
+        self.logger.debug('----------testInsertingOneDoc----------')
         testDoc = {
           'string': 'string value',
           'num': 99,
@@ -18,9 +34,9 @@ class MongoDalTest(unittest.TestCase):
           'subDoc': {'string1': 'str1', 'str2': 'str2'}
         }
         #TODO: Error Handle
-        global dal
-        id = dal.insertDoc(testDoc)
+        id = self.dal.insertDoc(testDoc)
         self.assertTrue(isinstance(id, ObjectId))
+        self.logger.debug('----------testInsertingOneDoc----------\n')
 
 if __name__ == '__main__':
     unittest.main()
