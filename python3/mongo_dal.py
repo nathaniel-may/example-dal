b06e8461c3ba487f338658ec1598507a9e7f1a0f
nathaniel may <nathanieldmay@gmail.com>
2018-02-06 12:10:46 -0500

    pep8 var name and comment format fixes

--- python3/mongo_dal.py
+++ python3/mongo_dal.py
@@ -3,23 +3,24 @@ from pymongo.errors import PyMongoError, ConnectionFailure, ServerSelectionTimeo
 from pymongo.write_concern import WriteConcern
 from pymongo.read_concern import ReadConcern
 from bson.objectid import ObjectId
-from datetime import datetime
 import logging.config
 
-''' Example functions for accessing mongodb.
-    All operations can be safely retried in the face of network errors.
+''' An example class for accessing mongodb
+
+    All operations are written so they can be safely retried 
+    in the event of network errors and primary failovers
 '''
 
 
 class MongoDal:
 
-    def __init__(self, connString, logLevel, logger_name='DAL'):
+    def __init__(self, conn_string, log_level, logger_name='DAL'):
 
         # logging
         self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logLevel)
+        self.logger.setLevel(log_level)
         handler = logging.StreamHandler()
-        handler.setLevel(logLevel)
+        handler.setLevel(log_level)
         formatter = logging.Formatter(
             '%(asctime)s\t%(name)s\t\t%(levelname)s::%(message)s',
             datefmt='%m/%d/%Y %H:%M:%S')
@@ -27,7 +28,7 @@ class MongoDal:
         self.logger.addHandler(handler)
 
         # set variables for connecting to mongo
-        self.connString = connString
+        self.conn_string = conn_string
         # setting flag for connection assert in later functions
         # self.connect must be called after init for error handling
         self.__connected = False
@@ -37,7 +38,7 @@ class MongoDal:
         # and there should only ever be one instance of MongoClient
         # Connect must be called after creating this to error handle
         # initial connection issues
-        self.client = MongoClient(self.connString, w='majority')
+        self.client = MongoClient(self.conn_string, w='majority')
 
         # write concern is redundant here because it is  set on the 
         # client connection above. colleciton defined write concern
@@ -54,15 +55,15 @@ class MongoDal:
         self.logger.debug('completed __init__')
 
     def connect(self):
+        """ Connects to the database. Raises errors caused by reads with an
+            incorrectly configured connection string. Errors relating to write options
+            such as write concern are only raised when a write is attempted
+            
+            Must be called once before calling any other function
+            
+            Calling twice raises an error
         """
-        Connects to the database. Raises errors caused by reads with an
-        incorrectly configured connection string. Errors relating to write options
-        such as write concern are only raised when a write is attempted
 
-        Must be called once before calling any other function
-
-        Calling twice raises an error
-        """
         # connect to mongodb
         self.logger.debug('started connect')
 
@@ -76,7 +77,7 @@ class MongoDal:
             self.logger.debug('testing connection to mongodb')
 
             # calling server_info() will catch a bad connection string and
-            # an unavailable unavilable cluster
+            # an unavailable cluster
             self.client.server_info()
             
             # setting flag for connection assert in later functions
@@ -85,12 +86,12 @@ class MongoDal:
         except ConnectionFailure as e:
             self.logger.fatal(
                 'Connection refused to {!s}.'.format(
-                    self.connString))
+                    self.conn_string))
             raise DbConnectionRefusedError(e)
         except ServerSelectionTimeoutError as e:
             self.logger.fatal(
                 'Server selection timeout. Are these servers reachable? {!s}'.format(
-                    self.connString))
+                    self.conn_string))
             raise DatabaseConnectionError(e)
         except PyMongoError as e:
             self.logger.fatal('error connecting: {!s}'.format(e))
@@ -100,15 +101,15 @@ class MongoDal:
             raise
 
     def __assert_connected(self):
-        """Raises DbNotConnectedError if connect has not been called"""
+        """ Raises DbNotConnectedError if connect has not been called """
 
         if not self.__connected:
             raise DbNotConnectedError()
 
     def close(self):
-        """Closes the connection to MongoDB.
+        """ Closes the connection to MongoDB.
 
-        Connection can be reinitialized by calling self.connect.
+            Connection can be reinitialized by calling self.connect.
         """
 
         self.__assert_connected()
@@ -124,9 +125,10 @@ class MongoDal:
             raise
 
     def insert_doc(self, doc):
-        """function for inserting a document.
+        """ function for inserting a document.
 
-        Assigns an id to the document if it does not already exist to make it safe to retry"""
+            Assigns an id to the document if it does not already exist to make it safe to retry
+        """
 
         self.__assert_connected()
         self.logger.debug('started insert_doc')
@@ -158,7 +160,7 @@ class MongoDal:
             raise
 
     def get_by_id(self, id):
-        """Gets a document by id"""
+        """ Gets a document by id """
 
         self.__assert_connected()
         self.logger.debug('started get_by_id')
@@ -179,9 +181,9 @@ class MongoDal:
     # this uses the algorithm outlined here to achieve idempotency:
     # https://explore.mongodb.com/developer/nathaniel-may
     def inc_counter(self, id):
-        """Increments the counter field of the document.
+        """ Increments the counter field of the document.
 
-        Appends ObjectIds to the document in the opids array to achieve idempotency.
+            Appends ObjectIds to the document in the opids array to achieve idempotency.
         """
 
         self.__assert_connected()
@@ -220,7 +222,7 @@ class MongoDal:
             raise
 
     def delete_all_docs(self):
-        """Deletes all documents in the collection. Does not drop the collection"""
+        """ Deletes all documents in the collection. Does not drop the collection """
 
         self.__assert_connected()
         self.logger.debug('started delete_all_docs')
@@ -237,26 +239,26 @@ class MongoDal:
             raise
 
     def __retry_on_error(self, fn, *args, **kwargs):
-        """This function exists to compensate for when network errors and primary
-           fail overs happen during our operation. A network error can occur on
-           the way to the database so that our operation never arrived, or on the
-           way back so that we do not know the operation took place. Making all
-           operations safe to retry and retrying them exactly once in the face of
-           these network errors prevents raising unnecessary errors to the user.
-
-           In v3.6 of the driver this functionality is built in for operations on
-           single documents with retryable writes.
+        """ This function exists to compensate for when network errors and primary
+            fail overs happen during our operation. A network error can occur on
+            the way to the database so that our operation never arrived, or on the
+            way back so that we do not know the operation took place. Making all
+            operations safe to retry and retrying them exactly once in the face of
+            these network errors prevents raising unnecessary errors to the user.
+            
+            In v3.6 of the driver this functionality is built in for operations on
+            single documents with retryable writes.
         """
 
         try:
             return fn(*args, **kwargs)
-        except ConnectionFailure as netErr:  # NetworkError base class
+        except ConnectionFailure:
             self.logger.debug('experienced network error- retrying')
             try:
                 return fn(*args, **kwargs)
             # the only way for a duplicate key error to happen here is if the
             # first insert succeeded
-            except DuplicateKeyError as e:
+            except DuplicateKeyError:
                 self.logger.debug('retry resolved network error')
             except Exception:
                 self.logger.error('could not resolve with retry')
@@ -268,16 +270,16 @@ class MongoDal:
 
 
 class DatabaseError(Exception):
-    """Base class for custom exceptions
-       this prevents higher functions from needing to catch pymongo-specific errors
+    """ Base class for custom exceptions
+        this prevents higher functions from needing to catch pymongo-specific errors
     """
 
     pass
 
 
 class DbNotConnectedError(DatabaseError):
-    """Exception raised when a database action has been called but
-       self.connect has not been called which has important error handling
+    """ Exception raised when a database action has been called but
+        self.connect has not been called which has important error handling
 
     Attributes:
         message -- generated explanation of the error
@@ -286,8 +288,9 @@ class DbNotConnectedError(DatabaseError):
     def __init__(self):
         self.message = 'must call connect after init'
 
+
 class DbAlreadyConnectedError(DatabaseError):
-    """Exception raised when self.connect has been called twice
+    """ Exception raised when self.connect has been called twice
 
     Attributes:
         message -- generated explanation of the error
@@ -298,18 +301,18 @@ class DbAlreadyConnectedError(DatabaseError):
 
 
 class DbConnectionRefusedError(DatabaseError):
-    """Exception raise when connection is refused to mongodb
+    """ Exception raise when connection is refused to mongodb
 
     Attributes:
         message -- generated explanation of the error
     """
 
     def __init__(self):
-        self.message = 'connection refused to '.format(self.connString)
+        self.message = 'connection refused to '.format(self.conn_string)
 
 
 class DbDuplicateIdError(DatabaseError):
-    """Exception raised when attempting to insert a document which contains
+    """ Exception raised when attempting to insert a document which contains
        an _id which is already present in the collection
 
     Attributes:
@@ -323,7 +326,7 @@ class DbDuplicateIdError(DatabaseError):
 
 
 class DbWrappedError(DatabaseError):
-    """Exception wraps any unexpected pymongo errors before raising
+    """ Exception wraps any unexpected pymongo errors before raising
 
     Attributes:
         err -- the original error being wrapped
